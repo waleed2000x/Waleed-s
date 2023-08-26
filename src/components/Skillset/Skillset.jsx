f3394e65e29da4c074afe5ed41e1fd5343dfa21c
waleed2000× <waleedahmed2000x@gmail.com>
2023-08-26 21:15:45 +0500

    HI

--- src/components/Skillset/Skillset.jsx
+++ src/components/Skillset/Skillset.jsx
@@ -25,7 +25,7 @@ export default function Skillset() {
       <div className="slidersParent">
         <div className="sliders" data-aos="fade-right" data-aos-duration="1000">
           <div className="sliderTitle">
-            <p>HTMLh</p>
+            <p>HTML</p>
             <div className="imageSkill">
               <img src={Html} alt="HTML" />
             </div>
