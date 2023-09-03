5e6143d016bce673a0def6233baec75fc55ba146
waleed2000× <waleedahmed2000x@gmail.com>
2023-09-03 17:07:23 +0500

    Contact page layout done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,19 @@
 import ContactLottie from "./contactLottie";
-
+import { Element } from "react-scroll";
 export default function Contact() {
   return (
-    <>
-      <div>contact</div>
-      <ContactLottie />
-    </>
+    <Element name="contact" className="contactParent">
+      <div className="lottieContact">
+        <ContactLottie />
+      </div>
+      <div className="contactForm">
+        <div className="leftContact">
+          <div className="contactTitle">
+            <p>Contact .</p>
+          </div>
+        </div>
+        <div className="rightContact"></div>
+      </div>
+    </Element>
   );
 }
--- src/components/contact/contactSchema.jsx
+++ src/components/contact/contactSchema.jsx
@@ -1,3 +1,11 @@
 import * as Yup from "yup";
 
-export const contactSchema = Yup.object({});
+export const ContactSchema = Yup.object({
+  fullname: Yup.string().min(2).required("Name is a reqiured field"),
+  email: Yup.string()
+    .email()
+    .min(4)
+    .max(25)
+    .required("Email is a reqiured field"),
+  message: Yup.string().max(200).required("Message is a reqiured field"),
+});
