eeecc19be9dc9f55612938bbc6547c06d8881d3e
waleed2000× <waleedahmed2000x@gmail.com>
2023-09-19 14:28:56 +0500

    Footer ~~~

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,39 @@
+import { motion } from "framer-motion";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import MailIcon from "@mui/icons-material/Mail";
+
 export default function Footer() {
   return (
     <div className="footerParent">
-      <p>Waleed</p>
+      <motion.h1
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.2, ease: "easeInOut" }}
+      >
+        <span className="left-animation">&lt;</span>
+        Waleed Ahmed
+        <span className="right-animation">&nbsp;/&gt;&nbsp;</span>
+      </motion.h1>
+      <div className="socialIcons">
+        <motion.div
+          whileHover={{ scale: "1.1" }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        >
+          <InstagramIcon />
+        </motion.div>
+        <motion.div
+          whileHover={{ scale: "1.1" }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        >
+          <LinkedInIcon />
+        </motion.div>
+        <motion.div
+          whileHover={{ scale: "1.1" }}
+          transition={{ duration: 0.3, ease: "easeInOut" }}
+        >
+          <MailIcon />
+        </motion.div>
+      </div>
     </div>
   );
 }
