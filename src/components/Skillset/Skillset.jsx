b7053e6e1d8ee9e31952583f492fab515be1d619
waleed2000× <waleedahmed2000x@gmail.com>
2023-10-06 23:07:38 +0500

    ~All Images And Skills Updated

--- src/components/Skillset/Skillset.jsx
+++ src/components/Skillset/Skillset.jsx
@@ -4,10 +4,15 @@ import Html from "./Images/html.svg";
 import CSS from "./Images/CSS.png";
 import JS from "./Images/JS.avif";
 import ReactLottie from "../Home/HomeLotties/ReactLottie";
+import Next from "./Images/next.svg";
+import NextAuth from "./Images/nextauth.png";
+import Mongo from "./Images/mongo.svg";
+import Express from "./Images/express.svg";
 import Node from "./Images/Node.png";
 import SCSS from "./Images/SCSS.png";
 import Bootstrap from "./Images/Bootstrap.png";
 import MUI from "./Images/MUI.png";
+import ChakraUI from "./Images/chakra.png";
 import Firebase from "./Images/Firebase.png";
 import Strapi from "./Images/Strapi.webp";
 import Framer from "./Images/Framer.png";
@@ -49,7 +54,7 @@ export default function Skillset() {
           </div>
           <Slider
             style={{ color: "yellow" }}
-            value={80}
+            value={95}
             valueLabelDisplay="auto"
           />
         </div>
@@ -77,12 +82,72 @@ export default function Skillset() {
         </div>
         <div className="sliders" data-aos="fade-left" data-aos-duration="1000">
           <div className="sliderTitle">
+            <p>Next</p>
+            <div className="imageSkill">
+              <img
+                src={Next}
+                style={{
+                  backgroundColor: "white",
+                  borderRadius: "50%",
+                  border: "2px solid black",
+                }}
+                alt="JS"
+              />
+            </div>
+          </div>
+          <Slider
+            style={{ color: "white" }}
+            value={70}
+            valueLabelDisplay="auto"
+          />
+        </div>
+        <div className="sliders" data-aos="fade-left" data-aos-duration="1000">
+          <div className="sliderTitle">
             <p>Node</p>
             <div className="imageSkill">
               <img src={Node} alt="JS" />
             </div>
           </div>
-          <Slider color="success" value={70} valueLabelDisplay="auto" />
+          <Slider color="success" value={90} valueLabelDisplay="auto" />
+        </div>
+        <div className="sliders" data-aos="fade-left" data-aos-duration="1000">
+          <div className="sliderTitle">
+            <p>Express</p>
+            <div className="imageSkill">
+              <img src={Express} alt="JS" />
+            </div>
+          </div>
+          <Slider
+            style={{ color: "grey" }}
+            value={80}
+            valueLabelDisplay="auto"
+          />
+        </div>
+        <div className="sliders" data-aos="fade-left" data-aos-duration="1000">
+          <div className="sliderTitle">
+            <p>Mongo DB</p>
+            <div className="imageSkill">
+              <img src={Mongo} alt="JS" />
+            </div>
+          </div>
+          <Slider
+            style={{ color: "green" }}
+            value={90}
+            valueLabelDisplay="auto"
+          />
+        </div>
+        <div className="sliders" data-aos="fade-left" data-aos-duration="1000">
+          <div className="sliderTitle">
+            <p>NextAuth</p>
+            <div className="imageSkill">
+              <img src={NextAuth} alt="NexuAuth" />
+            </div>
+          </div>
+          <Slider
+            style={{ color: "green" }}
+            value={90}
+            valueLabelDisplay="auto"
+          />
         </div>
         <div className="sliders" data-aos="fade-right" data-aos-duration="1000">
           <div className="sliderTitle">
@@ -91,7 +156,16 @@ export default function Skillset() {
               <img src={MUI} alt="JS" />
             </div>
           </div>
-          <Slider value={70} valueLabelDisplay="auto" />
+          <Slider value={90} valueLabelDisplay="auto" />
+        </div>
+        <div className="sliders" data-aos="fade-right" data-aos-duration="1000">
+          <div className="sliderTitle">
+            <p>Chakra UI</p>
+            <div className="imageSkill">
+              <img src={ChakraUI} alt="chakraUI" />
+            </div>
+          </div>
+          <Slider value={80} valueLabelDisplay="auto" />
         </div>
         <div className="sliders" data-aos="fade-left" data-aos-duration="1000">
           <div className="sliderTitle">
