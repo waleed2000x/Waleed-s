438e3f0c58ee049f61cf724966703a66f00e730c
waleed2000x <waleedahmed2000x@gmail.com>
2023-12-08 15:11:43 +0500

    Content Updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,20 +10,20 @@ export default function About() {
       </div>
       <h1>Frontend React Developer</h1>
       <p>
-        That's an excellent introduction and overview of your skills and passion
-        as a front-end React developer! It effectively highlights your
-        proficiency and dedication in turning designs into engaging web
-        experiences. The mention of your expertise in HTML, CSS, JavaScript, and
-        React, along with your ability to create responsive interfaces,
-        showcases your technical abilities. The emphasis on user-centered design
-        and creating seamless, performant applications demonstrates your
-        commitment to providing a high-quality user experience.
+        Passionate about front-end development, I specialize in bringing designs
+        to life as a React developer. Proficient in HTML, CSS, JavaScript, and
+        React, I seamlessly translate concepts into engaging web experiences. My
+        commitment to user-centered design ensures the creation of high-quality,
+        responsive interfaces, while my dedication to performance guarantees the
+        delivery of seamless applications. Elevate your digital presence with my
+        expertise in turning ideas into captivating, user-friendly realities.
       </p>
       <p style={{ marginTop: "0px" }}>
-        Additionally, my capabilities in working with tools like Strapi and
-        Firebase add a valuable layer to my skill set, allowing to build dynamic
-        and database-driven web applications. Mentioning collaboration and
-        bringing ideas to life shows enthusiasm for teamwork and creativity.
+        Additionally, my capabilities in working with tools like Strapi,
+        NextAuth, MUI, AWS and Firebase add a valuable layer to my skill set,
+        allowing to build dynamic and database-driven web applications.
+        Mentioning collaboration and bringing ideas to life shows enthusiasm for
+        teamwork and creativity.
       </p>
     </Element>
   );
